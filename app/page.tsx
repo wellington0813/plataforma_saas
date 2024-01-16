0a5bdc6053094b9dd6c6205c0060e335721529ff
wellington0813 <2607wellington@gmail.com>
2024-01-15 21:38:31 -0300

    Feat: adicionado navbar botoes e nova img na home

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
       {/* Feature section */}
       <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
         <div className='mb-12 px-6 lg:px-8'>
-          <div className='mx-auto max-w-2xl'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
             <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
               Comece a conversar em minutos
             </h2>
@@ -96,7 +96,7 @@ export default function Home() {
           <li className='md:flex-1'>
             <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
               <span className='text-sm font-medium text-blue-600'>
-                Step 1
+                Passo 1
               </span>
               <span className='text-xl font-semibold'>
                 Crie sua conta agora mesmo
@@ -108,12 +108,51 @@ export default function Home() {
             </div>
           </li>
 
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Passo 2
+              </span>
+              <span className='text-xl font-semibold'>
+                Carregue seu arquivo PDF
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Processaremos seu arquivo e o deixaremos pronto para você conversar.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Passo 3
+              </span>
+              <span className='text-xl font-semibold'>
+                Comece a fazer perguntas
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                É simples assim. Experimente o SAF hoje - leva menos de um minuto.
+              </span>
+            </div>
+          </li>
         </ol>
+
+        <div className='mx-auto max-w'>
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/file-upload-preview.jpg'
+                  alt='product preview'
+                  width={1419}
+                  height={732}
+                  quality={100}
+                  className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-
-
-
     </>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import  Link  from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs"
+const Navbar = () => {
+    return (
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+                    <Link
+                        href='/'
+                        className='flex z-40 font-semibold'>
+                        <span>SAF.</span>
+                    </Link>
+
+                    <div className="hidden itens-center space-x-4 sm:flex">
+                        <>
+                            <Link 
+                                href='/pricing'
+                                className={buttonVariants({
+                                variant: 'ghost',
+                                size: 'sm',
+                            })}>
+                                Planos
+                            </Link>
+                            <LoginLink 
+                                className={buttonVariants({
+                                variant: 'ghost',
+                                size: 'sm',
+                            })}>
+                             Entrar
+                            </LoginLink>
+                            <RegisterLink 
+                                className={buttonVariants({
+                                variant: 'ghost',
+                                size: 'sm',
+                            })}>
+                             Iniciar
+                            </RegisterLink>
+                        </>
+                    </div>
+
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    )
+}
+
+export default Navbar
